f5d49108ee3b4b2ac08341837dd97175a0acfbb3
Benji Lee <pnhbenji@gmail.com>
2016-08-04 19:08:56 -0500

    Finish log in system

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   
-  get 'users/new'
-
   root 'static_pages#home'
-
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
-  get 'session/new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804184227) do
+ActiveRecord::Schema.define(version: 20160804231233) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "remember_digest"
   end
 
 end
